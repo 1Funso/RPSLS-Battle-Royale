18e8c1bfc9057ae71cfc67f7ae18e399df9c80a9
1Funso <>
2023-06-24 14:50:05 +0000

    creation of inital functions in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/* Initialize global variables for player's name, number of rounds played, score and player choices */
+let playerName;
+let rounds = 0;
+let score = 0;
+let playerChoices = [];
+
+/* Function to start the game */
+function startGame() {
+
+}
+
+/* Function to play a round of the game */
+function playRound(playerChoice) {
+
+}
+
+
+/* Function to reset the game */
+function resetGame() {
+
+}
+
+/* Function to check the winner */
+function checkWinner(playerChoice, computerChoice) {
+
+}
